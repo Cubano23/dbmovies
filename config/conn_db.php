5597353468619dc08936515f9c2fb1395de3258b
soares <soares@MacBook-Pro.home>
2020-09-02 19:34:42 +0200

    reglages base de donnee

--- config/conn_db.php
+++ config/conn_db.php
@@ -12,7 +12,7 @@ class ConnDB
     {
         try
         {
-            $this->conn = new PDO("mysql:host=localhost;dbname=dbmovies", 'root', 'root');
+            $this->conn = new PDO("mysql:host=localhost;dbname=bruno-soares", 'bruno-soares', 'LegTux14122302!');
             //On définit le mode d'erreur de PDO sur Exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $this->conn;
